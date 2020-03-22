5d8922f424dc537f3d9f3859df258b839b99aa17
koffeeya <kavya.beheraj@gmail.com>
2020-03-22 15:27:45 -0400

    started js

---
+++ Project 01 Exploratory/chart.js
@@ -0,0 +1,8 @@
+class Chart {
+
+    constructor(){}
+    
+    draw(){}
+}
+
+export { Chart };
---
+++ Project 01 Exploratory/main.js
@@ -0,0 +1,54 @@
+// Import components
+import { Chart } from "./chart.js";
+
+let chart;
+
+// Set global state variables
+let state = {
+    data: [],
+    selectedTitle: null,
+    selectedYear: null,
+    selectedArtist: null,
+    selectedGender: null,
+}
+
+// Read in data
+d3.csv("data/artworks.csv", d3.autoType).then(
+    data => {
+        console.log("data", data);
+        state.data = data;
+        init();
+    }
+);
+
+
+// Init
+function init() {
+
+    const waffle = d3.select('.waffle');
+
+    console.log("R", )
+
+    waffle
+	.selectAll('.block')
+	.data(state.data, d => d.ObjectID)
+	.enter()
+	.append('div')
+	.attr('class', 'block')
+	.style('background-color', "white");
+
+}
+
+// Draw
+function draw() {
+
+}
+
+
+// Global State utility function
+function setGlobalState(nextState) {
+    state = { ...state, ...nextState };
+    console.log("new state:", state);
+    draw();
+  }
+  
